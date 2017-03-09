4bc93891e4f14103b7bd62e930cc349b31e7766e
Sean Cali <seankenjicali@gmail.com>
2017-03-09 12:42:07 -0800

    Todo App Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
-angular.module('VSTDA', []);
+angular.module('VSTDL', []);
 
 angular
-	.module('VSTDA')
+	.module('VSTDL')
 	.controller('todoApp', todoApp);
 
 function todoApp() {
 
   var vm = this;
- 	vm.todos = [];
-	vm.addTodo = function(name) {
-		vm.todos.push({text:vm.todoName});
-		vm.todoName = '';
-		//<li class="list-group-item list-group-item-danger">High Priority</li>
 
+	vm.order = "";
+	vm.priorities=[
+		["High", 1, "list-group-item list-group-item-danger"],
+		["Medium", 2, "list-group-item list-group-item-warning"],
+		["Low", 3, "list-group-item list-group-item-success"]
+	];
+ 	vm.todos = [];
+	vm.addTodo = function() {
+		vm.todos.push([vm.todoName, vm.priority[1], vm.priority[2]]);
+		vm.todoName = "";
+		vm.priority= "";
 	}
 
+	vm.byHigh = function() {
+		vm.order="1";
+	}
+	vm.byLow = function() {
+		vm.order="-1";
+	}
+	vm.byAlphabet = function() {
+		vm.order="0";
+	}
 
 };
